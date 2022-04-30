c2fecbe84e6860a9f85a07abd81f2ae2b70c20e9
Emre Can Sari <cansarrr2@gmail.com>
2022-04-30 14:24:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Dikdortgen dikdortgen1 = new Dikdortgen();
+        dikdortgen1.kisaKenar = 3;
+        dikdortgen1.uzunKenar = 4;
+        Console.WriteLine("Alan Hesabı : {0}",dikdortgen1.AlanHesabi());
+        Dikdortgen_struct dikdortgen;
+        dikdortgen.kisaKenar = 4;
+        dikdortgen.uzunKenar = 5;
+        Console.WriteLine("Alan Hesabı : {0}",dikdortgen.AlanHesabi());
+    }
+}
+class Dikdortgen
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long AlanHesabi()
+    {
+        return kisaKenar * uzunKenar;
+    }
+}
+struct Dikdortgen_struct
+{
+    public int kisaKenar;
+    public int uzunKenar;
+    public long AlanHesabi()
+    {
+        return kisaKenar * uzunKenar;
+    }
+}
